MiracleDROID-HnT/android_device_xiaomi_mido

mido: XiaomiParts: Refactor Sound Control

XiaomiParts/src/com/mdroid/settings/device/Startup.java
@@ -25,6 +25,10 @@ import android.support.v7.preference.PreferenceManager;
 import android.provider.Settings;
 import android.text.TextUtils;
 
+import com.mdroid.settings.device.sound.HeadphoneGainPreference;
+import com.mdroid.settings.device.sound.SpeakerGainPreference;
+import com.mdroid.settings.device.sound.MicGainPreference;
+
 public class Startup extends BroadcastReceiver {
 
     private void restore(String file, boolean enabled) {

XiaomiParts/src/com/mdroid/settings/device/sound/HeadphoneGainPreference.java
@@ -15,7 +15,7 @@
 * along with this program. If not, see <http://www.gnu.org/licenses/>.
 *
 */
-package com.mdroid.settings.device;
+package com.mdroid.settings.device.sound;
 
 import android.content.Context;
 import android.content.SharedPreferences;
@@ -25,15 +25,18 @@ import android.util.AttributeSet;
 
 import java.util.List;
 
-public class SpeakerGainPreference extends ProperSeekBarPreference {
+import com.mdroid.settings.device.ProperSeekBarPreference;
+import com.mdroid.settings.device.Utils;
 
-    private static int mMinVal = -10;
+public class HeadphoneGainPreference extends ProperSeekBarPreference {
+
+    private static int mMinVal = -84;
     private static int mMaxVal = 20;
     private static int mDefVal = 0;
 
-    private static final String SPEAKER_GAIN_PATH = "/sys/kernel/sound_control/speaker_gain";
+    private static final String HEADPHONE_GAIN_PATH = "/sys/kernel/sound_control/headphone_gain";
 
-    public SpeakerGainPreference(Context context, AttributeSet attrs) {
+    public HeadphoneGainPreference(Context context, AttributeSet attrs) {
         super(context, attrs);
 
         mInterval = 1;
@@ -44,12 +47,13 @@ public class SpeakerGainPreference extends ProperSeekBarPreference {
         mMaxValue = mMaxVal;
         mDefaultValueExists = true;
         mDefaultValue = mDefVal;
+        mValue = Integer.parseInt(loadValue());
 
         setPersistent(false);
     }
 
     public static boolean isSupported() {
-        return Utils.fileWritable(SPEAKER_GAIN_PATH);
+        return Utils.fileWritable(HEADPHONE_GAIN_PATH);
     }
 
     public static void restore(Context context) {
@@ -57,14 +61,25 @@ public class SpeakerGainPreference extends ProperSeekBarPreference {
             return;
         }
 
-        String storedValue = PreferenceManager.getDefaultSharedPreferences(context).getString(DeviceSettings.KEY_SPEAKER_GAIN, String.valueOf(mDefVal));
-        Utils.writeValue(SPEAKER_GAIN_PATH, storedValue);
+        String storedValue = PreferenceManager.getDefaultSharedPreferences(context).getString(SoundControlSettings.KEY_HEADPHONE_GAIN, String.valueOf(mDefVal));
+        Utils.writeValue(HEADPHONE_GAIN_PATH, storedValue + " " + storedValue);
+    }
+
+    public static String loadValue() {
+        String storedValue = Utils.getFileValue(HEADPHONE_GAIN_PATH, String.valueOf(mDefVal));
+        int value = Integer.valueOf(storedValue.substring(0, storedValue.indexOf(' ')));
+        if (value >= 0 && value <= 20) {
+            return String.valueOf(value);
+        } else if (value >= 172 && value <= 255) {
+            return String.valueOf(value - 256);
+        }
+        return "";
     }
 
     private void saveValue(String newValue) {
-        Utils.writeValue(SPEAKER_GAIN_PATH, newValue);
+        Utils.writeValue(HEADPHONE_GAIN_PATH, newValue + " " + newValue);
         SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(getContext()).edit();
-        editor.putString(DeviceSettings.KEY_SPEAKER_GAIN, newValue);
+        editor.putString(SoundControlSettings.KEY_HEADPHONE_GAIN, newValue);
         editor.commit();
     }
 

XiaomiParts/src/com/mdroid/settings/device/sound/MicGainPreference.java
@@ -15,7 +15,7 @@
 * along with this program. If not, see <http://www.gnu.org/licenses/>.
 *
 */
-package com.mdroid.settings.device;
+package com.mdroid.settings.device.sound;
 
 import android.content.Context;
 import android.content.SharedPreferences;
@@ -24,6 +24,9 @@ import android.support.v7.preference.PreferenceViewHolder;
 import android.util.AttributeSet;
 
 import java.util.List;
+
+import com.mdroid.settings.device.ProperSeekBarPreference;
+import com.mdroid.settings.device.Utils;
 
 public class MicGainPreference extends ProperSeekBarPreference {
 
@@ -44,6 +47,7 @@ public class MicGainPreference extends ProperSeekBarPreference {
         mMaxValue = mMaxVal;
         mDefaultValueExists = true;
         mDefaultValue = mDefVal;
+        mValue = Integer.parseInt(loadValue());
 
         setPersistent(false);
     }
@@ -57,14 +61,24 @@ public class MicGainPreference extends ProperSeekBarPreference {
             return;
         }
 
-        String storedValue = PreferenceManager.getDefaultSharedPreferences(context).getString(DeviceSettings.KEY_MICROPHONE_GAIN, String.valueOf(mDefVal));
+        String storedValue = PreferenceManager.getDefaultSharedPreferences(context).getString(SoundControlSettings.KEY_MICROPHONE_GAIN, String.valueOf(mDefVal));
         Utils.writeValue(MICROPHONE_GAIN_PATH, storedValue);
+    }
+
+    public static String loadValue() {
+        int value = Integer.valueOf(Utils.getFileValue(MICROPHONE_GAIN_PATH, String.valueOf(mDefVal)));
+        if (value >= 0 && value <= 20) {
+            return String.valueOf(value);
+        } else if (value >= 246 && value <= 255) {
+            return String.valueOf(value - 256);
+        }
+        return "";
     }
 
     private void saveValue(String newValue) {
         Utils.writeValue(MICROPHONE_GAIN_PATH, newValue);
         SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(getContext()).edit();
-        editor.putString(DeviceSettings.KEY_MICROPHONE_GAIN, newValue);
+        editor.putString(SoundControlSettings.KEY_MICROPHONE_GAIN, newValue);
         editor.commit();
     }
 

XiaomiParts/src/com/mdroid/settings/device/sound/SoundControlActivity.java
@@ -1,0 +1,61 @@
+/*
+* Copyright (C) 2017 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package com.mdroid.settings.device.sound;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.v14.preference.PreferenceFragment;
+import android.support.v7.preference.PreferenceManager;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class SoundControlActivity extends Activity {
+
+    private SoundControlSettings mSoundControlSettingsFragment;
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Fragment fragment = getFragmentManager().findFragmentById(android.R.id.content);
+        if (fragment == null) {
+            mSoundControlSettingsFragment = new SoundControlSettings();
+            getFragmentManager().beginTransaction()
+                .add(android.R.id.content, mSoundControlSettingsFragment)
+                .commit();
+        } else {
+            mSoundControlSettingsFragment = (SoundControlSettings) fragment;
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+        case android.R.id.home:
+            finish();
+            return true;
+        default:
+            break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}

XiaomiParts/src/com/mdroid/settings/device/sound/SoundControlSettings.java
@@ -1,0 +1,55 @@
+/*
+* Copyright (C) 2016 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package com.mdroid.settings.device.sound;
+
+import android.os.Bundle;
+import android.support.v14.preference.PreferenceFragment;
+import android.support.v7.preference.Preference;
+
+import com.mdroid.settings.device.R;
+
+public class SoundControlSettings extends PreferenceFragment {
+
+    public static final String KEY_HEADPHONE_GAIN = "headphone_gain";
+    public static final String KEY_SPEAKER_GAIN = "speaker_gain";
+    public static final String KEY_MICROPHONE_GAIN = "mic_gain";
+
+    private HeadphoneGainPreference mHeadphoneGain;
+    private SpeakerGainPreference mSpeakerGain;
+    private MicGainPreference mMicGain;
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.sound_control, rootKey);
+
+        mHeadphoneGain = (HeadphoneGainPreference) findPreference(KEY_HEADPHONE_GAIN);
+        if (mHeadphoneGain != null) {
+            mHeadphoneGain.setEnabled(HeadphoneGainPreference.isSupported());
+        }
+
+        mSpeakerGain = (SpeakerGainPreference) findPreference(KEY_SPEAKER_GAIN);
+        if (mSpeakerGain != null) {
+            mSpeakerGain.setEnabled(SpeakerGainPreference.isSupported());
+        }
+
+        mMicGain = (MicGainPreference) findPreference(KEY_MICROPHONE_GAIN);
+        if (mMicGain != null) {
+            mMicGain.setEnabled(MicGainPreference.isSupported());
+        }
+    }
+}

XiaomiParts/src/com/mdroid/settings/device/sound/SpeakerGainPreference.java
@@ -15,7 +15,7 @@
 * along with this program. If not, see <http://www.gnu.org/licenses/>.
 *
 */
-package com.mdroid.settings.device;
+package com.mdroid.settings.device.sound;
 
 import android.content.Context;
 import android.content.SharedPreferences;
@@ -25,15 +25,18 @@ import android.util.AttributeSet;
 
 import java.util.List;
 
-public class HeadphoneGainPreference extends ProperSeekBarPreference {
+import com.mdroid.settings.device.ProperSeekBarPreference;
+import com.mdroid.settings.device.Utils;
 
-    private static int mMinVal = -84;
+public class SpeakerGainPreference extends ProperSeekBarPreference {
+
+    private static int mMinVal = -10;
     private static int mMaxVal = 20;
     private static int mDefVal = 0;
 
-    private static final String HEADPHONE_GAIN_PATH = "/sys/kernel/sound_control/headphone_gain";
+    private static final String SPEAKER_GAIN_PATH = "/sys/kernel/sound_control/speaker_gain";
 
-    public HeadphoneGainPreference(Context context, AttributeSet attrs) {
+    public SpeakerGainPreference(Context context, AttributeSet attrs) {
         super(context, attrs);
 
         mInterval = 1;
@@ -44,12 +47,13 @@ public class HeadphoneGainPreference extends ProperSeekBarPreference {
         mMaxValue = mMaxVal;
         mDefaultValueExists = true;
         mDefaultValue = mDefVal;
+        mValue = Integer.parseInt(loadValue());
 
         setPersistent(false);
     }
 
     public static boolean isSupported() {
-        return Utils.fileWritable(HEADPHONE_GAIN_PATH);
+        return Utils.fileWritable(SPEAKER_GAIN_PATH);
     }
 
     public static void restore(Context context) {
@@ -57,14 +61,24 @@ public class HeadphoneGainPreference extends ProperSeekBarPreference {
             return;
         }
 
-        String storedValue = PreferenceManager.getDefaultSharedPreferences(context).getString(DeviceSettings.KEY_HEADPHONE_GAIN, String.valueOf(mDefVal));
-        Utils.writeValue(HEADPHONE_GAIN_PATH, storedValue + " " + storedValue);
+        String storedValue = PreferenceManager.getDefaultSharedPreferences(context).getString(SoundControlSettings.KEY_SPEAKER_GAIN, String.valueOf(mDefVal));
+        Utils.writeValue(SPEAKER_GAIN_PATH, storedValue);
+    }
+
+    public static String loadValue() {
+        int value = Integer.valueOf(Utils.getFileValue(SPEAKER_GAIN_PATH, String.valueOf(mDefVal)));
+        if (value >= 0 && value <= 20) {
+            return String.valueOf(value);
+        } else if (value >= 246 && value <= 255) {
+            return String.valueOf(value - 256);
+        }
+        return "";
     }
 
     private void saveValue(String newValue) {
-        Utils.writeValue(HEADPHONE_GAIN_PATH, newValue + " " + newValue);
+        Utils.writeValue(SPEAKER_GAIN_PATH, newValue);
         SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(getContext()).edit();
-        editor.putString(DeviceSettings.KEY_HEADPHONE_GAIN, newValue);
+        editor.putString(SoundControlSettings.KEY_SPEAKER_GAIN, newValue);
         editor.commit();
     }
 

getmyname/android_device_xiaomi_mido-btlg

XiaomiParts/src/com/lineageos/settings/device/Startup.java
@@ -25,6 +25,10 @@ import android.support.v7.preference.PreferenceManager;
 import android.provider.Settings;
 import android.text.TextUtils;
 
+import com.lineageos.settings.device.sound.HeadphoneGainPreference;
+import com.lineageos.settings.device.sound.SpeakerGainPreference;
+import com.lineageos.settings.device.sound.MicGainPreference;
+
 public class Startup extends BroadcastReceiver {
 
     private void restore(String file, boolean enabled) {

XiaomiParts/src/com/lineageos/settings/device/sound/HeadphoneGainPreference.java
@@ -15,7 +15,7 @@
 * along with this program. If not, see <http://www.gnu.org/licenses/>.
 *
 */
-package com.lineageos.settings.device;
+package com.lineageos.settings.device.sound;
 
 import android.content.Context;
 import android.content.SharedPreferences;
@@ -24,6 +24,9 @@ import android.support.v7.preference.PreferenceViewHolder;
 import android.util.AttributeSet;
 
 import java.util.List;
+
+import com.lineageos.settings.device.ProperSeekBarPreference;
+import com.lineageos.settings.device.Utils;
 
 public class HeadphoneGainPreference extends ProperSeekBarPreference {
 
@@ -44,6 +47,7 @@ public class HeadphoneGainPreference extends ProperSeekBarPreference {
         mMaxValue = mMaxVal;
         mDefaultValueExists = true;
         mDefaultValue = mDefVal;
+        mValue = Integer.parseInt(loadValue());
 
         setPersistent(false);
     }
@@ -57,14 +61,25 @@ public class HeadphoneGainPreference extends ProperSeekBarPreference {
             return;
         }
 
-        String storedValue = PreferenceManager.getDefaultSharedPreferences(context).getString(DeviceSettings.KEY_HEADPHONE_GAIN, String.valueOf(mDefVal));
+        String storedValue = PreferenceManager.getDefaultSharedPreferences(context).getString(SoundControlSettings.KEY_HEADPHONE_GAIN, String.valueOf(mDefVal));
         Utils.writeValue(HEADPHONE_GAIN_PATH, storedValue + " " + storedValue);
+    }
+
+    public static String loadValue() {
+        String storedValue = Utils.getFileValue(HEADPHONE_GAIN_PATH, String.valueOf(mDefVal));
+        int value = Integer.valueOf(storedValue.substring(0, storedValue.indexOf(' ')));
+        if (value >= 0 && value <= 20) {
+            return String.valueOf(value);
+        } else if (value >= 172 && value <= 255) {
+            return String.valueOf(value - 256);
+        }
+        return "";
     }
 
     private void saveValue(String newValue) {
         Utils.writeValue(HEADPHONE_GAIN_PATH, newValue + " " + newValue);
         SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(getContext()).edit();
-        editor.putString(DeviceSettings.KEY_HEADPHONE_GAIN, newValue);
+        editor.putString(SoundControlSettings.KEY_HEADPHONE_GAIN, newValue);
         editor.commit();
     }
 

XiaomiParts/src/com/lineageos/settings/device/sound/MicGainPreference.java
@@ -15,7 +15,7 @@
 * along with this program. If not, see <http://www.gnu.org/licenses/>.
 *
 */
-package com.lineageos.settings.device;
+package com.lineageos.settings.device.sound;
 
 import android.content.Context;
 import android.content.SharedPreferences;
@@ -24,6 +24,9 @@ import android.support.v7.preference.PreferenceViewHolder;
 import android.util.AttributeSet;
 
 import java.util.List;
+
+import com.lineageos.settings.device.ProperSeekBarPreference;
+import com.lineageos.settings.device.Utils;
 
 public class MicGainPreference extends ProperSeekBarPreference {
 
@@ -44,6 +47,7 @@ public class MicGainPreference extends ProperSeekBarPreference {
         mMaxValue = mMaxVal;
         mDefaultValueExists = true;
         mDefaultValue = mDefVal;
+        mValue = Integer.parseInt(loadValue());
 
         setPersistent(false);
     }
@@ -57,14 +61,24 @@ public class MicGainPreference extends ProperSeekBarPreference {
             return;
         }
 
-        String storedValue = PreferenceManager.getDefaultSharedPreferences(context).getString(DeviceSettings.KEY_MICROPHONE_GAIN, String.valueOf(mDefVal));
+        String storedValue = PreferenceManager.getDefaultSharedPreferences(context).getString(SoundControlSettings.KEY_MICROPHONE_GAIN, String.valueOf(mDefVal));
         Utils.writeValue(MICROPHONE_GAIN_PATH, storedValue);
+    }
+
+    public static String loadValue() {
+        int value = Integer.valueOf(Utils.getFileValue(MICROPHONE_GAIN_PATH, String.valueOf(mDefVal)));
+        if (value >= 0 && value <= 20) {
+            return String.valueOf(value);
+        } else if (value >= 246 && value <= 255) {
+            return String.valueOf(value - 256);
+        }
+        return "";
     }
 
     private void saveValue(String newValue) {
         Utils.writeValue(MICROPHONE_GAIN_PATH, newValue);
         SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(getContext()).edit();
-        editor.putString(DeviceSettings.KEY_MICROPHONE_GAIN, newValue);
+        editor.putString(SoundControlSettings.KEY_MICROPHONE_GAIN, newValue);
         editor.commit();
     }
 

XiaomiParts/src/com/lineageos/settings/device/sound/SoundControlActivity.java
@@ -1,0 +1,61 @@
+/*
+* Copyright (C) 2017 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package com.lineageos.settings.device.sound;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.v14.preference.PreferenceFragment;
+import android.support.v7.preference.PreferenceManager;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class SoundControlActivity extends Activity {
+
+    private SoundControlSettings mSoundControlSettingsFragment;
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Fragment fragment = getFragmentManager().findFragmentById(android.R.id.content);
+        if (fragment == null) {
+            mSoundControlSettingsFragment = new SoundControlSettings();
+            getFragmentManager().beginTransaction()
+                .add(android.R.id.content, mSoundControlSettingsFragment)
+                .commit();
+        } else {
+            mSoundControlSettingsFragment = (SoundControlSettings) fragment;
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+        case android.R.id.home:
+            finish();
+            return true;
+        default:
+            break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}

XiaomiParts/src/com/lineageos/settings/device/sound/SoundControlSettings.java
@@ -1,0 +1,55 @@
+/*
+* Copyright (C) 2016 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+package com.lineageos.settings.device.sound;
+
+import android.os.Bundle;
+import android.support.v14.preference.PreferenceFragment;
+import android.support.v7.preference.Preference;
+
+import com.lineageos.settings.device.R;
+
+public class SoundControlSettings extends PreferenceFragment {
+
+    public static final String KEY_HEADPHONE_GAIN = "headphone_gain";
+    public static final String KEY_SPEAKER_GAIN = "speaker_gain";
+    public static final String KEY_MICROPHONE_GAIN = "mic_gain";
+
+    private HeadphoneGainPreference mHeadphoneGain;
+    private SpeakerGainPreference mSpeakerGain;
+    private MicGainPreference mMicGain;
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.sound_control, rootKey);
+
+        mHeadphoneGain = (HeadphoneGainPreference) findPreference(KEY_HEADPHONE_GAIN);
+        if (mHeadphoneGain != null) {
+            mHeadphoneGain.setEnabled(HeadphoneGainPreference.isSupported());
+        }
+
+        mSpeakerGain = (SpeakerGainPreference) findPreference(KEY_SPEAKER_GAIN);
+        if (mSpeakerGain != null) {
+            mSpeakerGain.setEnabled(SpeakerGainPreference.isSupported());
+        }
+
+        mMicGain = (MicGainPreference) findPreference(KEY_MICROPHONE_GAIN);
+        if (mMicGain != null) {
+            mMicGain.setEnabled(MicGainPreference.isSupported());
+        }
+    }
+}

XiaomiParts/src/com/lineageos/settings/device/sound/SpeakerGainPreference.java
@@ -15,7 +15,7 @@
 * along with this program. If not, see <http://www.gnu.org/licenses/>.
 *
 */
-package com.lineageos.settings.device;
+package com.lineageos.settings.device.sound;
 
 import android.content.Context;
 import android.content.SharedPreferences;
@@ -24,6 +24,9 @@ import android.support.v7.preference.PreferenceViewHolder;
 import android.util.AttributeSet;
 
 import java.util.List;
+
+import com.lineageos.settings.device.ProperSeekBarPreference;
+import com.lineageos.settings.device.Utils;
 
 public class SpeakerGainPreference extends ProperSeekBarPreference {
 
@@ -44,6 +47,7 @@ public class SpeakerGainPreference extends ProperSeekBarPreference {
         mMaxValue = mMaxVal;
         mDefaultValueExists = true;
         mDefaultValue = mDefVal;
+        mValue = Integer.parseInt(loadValue());
 
         setPersistent(false);
     }
@@ -57,14 +61,24 @@ public class SpeakerGainPreference extends ProperSeekBarPreference {
             return;
         }
 
-        String storedValue = PreferenceManager.getDefaultSharedPreferences(context).getString(DeviceSettings.KEY_SPEAKER_GAIN, String.valueOf(mDefVal));
+        String storedValue = PreferenceManager.getDefaultSharedPreferences(context).getString(SoundControlSettings.KEY_SPEAKER_GAIN, String.valueOf(mDefVal));
         Utils.writeValue(SPEAKER_GAIN_PATH, storedValue);
+    }
+
+    public static String loadValue() {
+        int value = Integer.valueOf(Utils.getFileValue(SPEAKER_GAIN_PATH, String.valueOf(mDefVal)));
+        if (value >= 0 && value <= 20) {
+            return String.valueOf(value);
+        } else if (value >= 246 && value <= 255) {
+            return String.valueOf(value - 256);
+        }
+        return "";
     }
 
     private void saveValue(String newValue) {
         Utils.writeValue(SPEAKER_GAIN_PATH, newValue);
         SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(getContext()).edit();
-        editor.putString(DeviceSettings.KEY_SPEAKER_GAIN, newValue);
+        editor.putString(SoundControlSettings.KEY_SPEAKER_GAIN, newValue);
         editor.commit();
     }
 